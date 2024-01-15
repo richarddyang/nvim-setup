aff88733b5b8b0d80fd552f2515379b2c51905d7
richarddyang <richard.yang@skywatertechnology.com>
2024-01-15 11:33:01 -0600

    update: add key map lookup hot key

--- init.lua
+++ init.lua
@@ -382,6 +382,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>km', require('telescope.builtin').keymaps, { desc = '[K]ey [M]aps' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
